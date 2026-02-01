e9156b81d372a30811d63601d35b83888cb6ea50
nadermkhan <muhammadnadermahbubkhan@gmail,com>
2026-02-01 16:13:14 +0600

    added admob plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,9 +7,19 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "navigator.splashscreen"
       ]
+    },
+    {
+      "id": "cordova-plugin-admob-plus.AdMob",
+      "file": "plugins/cordova-plugin-admob-plus/www/AdMob.js",
+      "pluginId": "cordova-plugin-admob-plus",
+      "clobbers": [
+        "window.AdMob"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-splashscreen": "6.0.1"
+    "cordova-plugin-splashscreen": "6.0.1",
+    "cordova-plugin-extension": "1.6.0",
+    "cordova-plugin-admob-plus": "1.3.2"
   };
 });
